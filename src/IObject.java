355480f0dd7c570ec7ea89b10d992c89554c4b2f
scottcarroll <stc4921@gmail.com>
2019-04-06 14:19:33 -0400

    Added abstract class

--- src/IObject.java
+++ src/IObject.java
@@ -4,5 +4,40 @@ package PACKAGE_NAME;
  * Created by Michael Goodnow on 2019-04-06.
  */
 
+/**
+ * This interface represents each object to be repesented in our map and allows functionality to
+ * return necessary information
+ */
 public interface IObject {
+
+  /**
+   * Return the objects X-Coordinate
+   * @return X-Coordinate
+   */
+  double getX();
+
+  /**
+   * Return the objects Y-Coordinate
+   * @return Y-Coordinate
+   */
+  double getY();
+
+  /**
+   * Return the objects Map Color
+   * @return The objects color on the map
+   */
+  Color getColor();
+
+  /**
+   * Return the objects type as a string
+   * @return The name of the object type
+   */
+  String getObjectType();
+
+  /**
+   * Returns information about the object if possible, returns NULL if no information about this
+   * object is avaliable
+   * @return
+   */
+  String getInformation();
 }
