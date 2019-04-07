da7bc4f9819d71f1cef9866a77b78037de03a02f
m-patil <mihirp288@gmail.com>
2019-04-07 18:33:46 -0400

    Made changes to AnimationPanel
    Added code that converts the latitude and longitude to the x and y pixel coordinates for the map

--- src/View/AnimationPanel.java
+++ src/View/AnimationPanel.java
@@ -22,6 +22,20 @@ public class AnimationPanel extends JPanel {
     this.Objects = list;
   }
 
+  private int longToX(double x) {
+    double temp1 = x % 0.00001;
+    temp1 = temp1 - 42.32881;
+    int temp2 = (int) temp1 * 10000;
+    return temp2;
+  }
+
+  private int latToY(double y) {
+    double temp1 = y % 0.00001;
+    temp1 = temp1 + 71.12411;
+    int temp2 = (int) temp1 * 10000;
+    return temp2;
+  }
+
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
@@ -29,8 +43,9 @@ public class AnimationPanel extends JPanel {
     Graphics2D g2d = (Graphics2D) g;
 
     for (IGeoObject i : Objects) {
-      int x = 0;
-      int y = 0;
+
+      int x = longToX(i.getX());
+      int y = latToY(i.getY());
 
       g2d.setColor(i.getColor());
       g2d.fillOval(x, y, 1, 1);
