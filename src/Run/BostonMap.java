575382da486605cc5f4ffb7a8a671303f45a5d27
trevarmand <trevor.armand13@verizon.net>
2019-04-18 14:53:55 -0400

    Added an entry point class for program

---
+++ src/Run/BostonMap.java
@@ -0,0 +1,21 @@
+package Run;
+
+import Model.DataTranslator;
+import Model.IDataTranslator;
+import View.AnimationPanel;
+import View.MapView;
+
+/**
+ * Entry point for our program.
+ */
+public class BostonMap {
+  private AnimationPanel ap;
+  private MapView mp;
+  private IDataTranslator dt;
+
+  public BostonMap() {
+    this.dt = new DataTranslator();
+    //this.mp = new MapView();
+    //this.mp.refresh();
+  }
+}
